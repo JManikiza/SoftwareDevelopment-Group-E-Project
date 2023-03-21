447c199307d2a83fb5c5f9f26aebc3f5afa139dc
Joven Manikiza <Jamanikiza@gmail.com>
2023-03-21 18:00:34 +0000

    Create app

---
+++ React + .GovPK/app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+import {
+  Button
+} from 'govuk-react';
+
+function App() {
+  return ( 
+    <div>
+      <Button>
+  Button
+</Button>
+    </div>
+  );
+}
+
+export default App;
