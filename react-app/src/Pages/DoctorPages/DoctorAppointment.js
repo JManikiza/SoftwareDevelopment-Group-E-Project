346259b6042fc329f415bbc89f92181f1fcb1ce8
notmirai <107251702+Mohamed-Y-Mohamed@users.noreply.github.com>
2023-05-04 18:44:15 +0100

    updated Doctor view appointment
    View appointment doctor has been updated with data retrieval and processing to the table depending on the number of appointment

--- react-app/src/Pages/DoctorPages/DoctorAppointment.js
+++ react-app/src/Pages/DoctorPages/DoctorAppointment.js
@@ -1,49 +1,122 @@
-
-import { Main,Table,Breadcrumbs,H3 } from "govuk-react";
+import { Main,Table,Breadcrumbs,H3,Button } from "govuk-react";
 import Navigation from "../../Components/Navigation";
+import jq from "jquery";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function DoctorAppointment() {
-    
-    return(
-        <div>
- <Navigation pageLink1="/" PageName1="Home" pageLink2="/DoctorDashBoard" PageName2="Doctor DashBoard" pageLink3="/DoctorViewandUpdate" PageName3="Doctor View and Update"/>
+  const navigate=useNavigate();
+  const [appointmentData, setAppointmentData] = useState([]);
 
-<Main>
+  useEffect(() => {
+    getPatientData();
+  }, []);
 
-<Breadcrumbs>
-  <Breadcrumbs.Link href="/">
-    HomePage
-  </Breadcrumbs.Link>
-  <Breadcrumbs.Link href="/DoctorDashBoard">
-  Doctor DashBoard</Breadcrumbs.Link>
-  <Breadcrumbs.Link href="/DoctorAppointment">
-  Doctor Appointment</Breadcrumbs.Link>
-  </Breadcrumbs>
-  <br/>  <br/>
-  <H3 size="LARGE" style={{textAlign: "center"}}>
-Doctor List of Sceduled Appointment </H3>
-<br/><br/><br/>
-<Table 
-  caption="Doctor Name: List Of  Appointments Booked "
-  
-  head={<Table.Row><Table.CellHeader >Patient NHS number </Table.CellHeader><Table.CellHeader>Patient Name </Table.CellHeader><Table.CellHeader numeric setWidth="12%" style={{ textAlign: "left" }}> Date</Table.CellHeader><Table.CellHeader numeric setWidth="10%" style={{ textAlign: "left" }}> Time</Table.CellHeader> <Table.CellHeader setWidth="30%" style={{ textAlign: "left" }}>Appointment Note</Table.CellHeader></Table.Row>}
->
-  <Table.Row>
-    <Table.CellHeader style={{ textAlign: "left" }}>
-      number
-    </Table.CellHeader>
-    <Table.Cell numeric  style={{ textAlign: "left" }} >
-patient name
-    </Table.Cell>
-    <Table.Cell numeric  style={{ textAlign: "left" }}>
-date 
-    </Table.Cell>
-    <Table.Cell numeric  style={{ textAlign: "left" }}>
-time 
-    </Table.Cell>
-    <Table.Cell numeric  style={{ textAlign: "left" }}>
-Appointments
-    </Table.Cell>
-  </Table.Row>
+  function getPatientData() {
+    var AppointmentData = {
+      empID: 1,
+    };
+
+    jq.ajax({
+      type: "POST",
+      url: "http://localhost:4000/DocViewAppointments.php",
+      mode: "no-core",
+      data: AppointmentData,
+      success(data) {
+        console.log(data);
+        if (data === "no patients") {
+          alert("no Appointment has been made")
+        } else {
+          var json = jq.parseJSON(data);
+          setAppointmentData(json);
+          
+        }
+      },
+    });
+  }
+
+  return(
+    <div>
+      <Navigation
+       pageLink1="/" PageName1="Home" pageLink2="/DoctorDashBoard"
+        PageName2="Doctor DashBoard" pageLink3="/DoctorViewandUpdate"
+         PageName3="Doctor View and Update"/>
+
+      <Main>
+        <Breadcrumbs>
+          <Breadcrumbs.Link href="/">
+            HomePage
+          </Breadcrumbs.Link>
+          <Breadcrumbs.Link href="/DoctorDashBoard">
+            Doctor DashBoard
+          </Breadcrumbs.Link>
+          <Breadcrumbs.Link href="/DoctorAppointment">
+            Doctor Appointment
+          </Breadcrumbs.Link>
+        </Breadcrumbs>
+        <br/>  <br/>
+     
+        {appointmentData.length > 0 && (
+  <H3 size="LARGE" style={{ textAlign: "center" }}>
+    {"Doctor " + appointmentData[0].empFName + " " + appointmentData[0].empLName}
+  </H3>
+)}
+          
+        <br/><br/><br/>
+        <Table  
+          caption={"List of Appointment"}
+          head={
+            <Table.Row>
+              <Table.CellHeader>Patient NHS number</Table.CellHeader>
+              <Table.CellHeader>Patient Name</Table.CellHeader>
+              <Table.CellHeader  setWidth="12%" style={{ textAlign: "left" }}> Date</Table.CellHeader>
+              <Table.CellHeader  setWidth="10%" style={{ textAlign: "left" }}> Time</Table.CellHeader>
+              <Table.CellHeader setWidth="30%" style={{ textAlign: "left" }}>Appointment Note</Table.CellHeader>
+              <Table.CellHeader setWidth="15%" style={{ textAlign: "left" }}>View medical Record</Table.CellHeader>
+
+            </Table.Row>
+          }
+        >
+      
+
+
+          {appointmentData.map((appointment) => (
+
+            <Table.Row>
+              
+              <Table.CellHeader style={{ textAlign: "left" }}>
+                {appointment.NHSNumber}
+              </Table.CellHeader>
+              <Table.Cell numeric  style={{ textAlign: "left" }} >
+              {appointment.Forename+" "+appointment.Surname}
+              </Table.Cell>
+              <Table.Cell numeric  style={{ textAlign: "left" }}>
+                {appointment.appDateAssigned}
+              </Table.Cell>
+              <Table.Cell numeric  style={{ textAlign: "left" }}>
+                {appointment.AppTimeAssigned}
+              </Table.Cell>
+              <Table.Cell   style={{ textAlign: "left" }}>
+                {appointment.appNotes}
+              </Table.Cell>
+              <Table.Cell numeric  style={{ textAlign: "left" }}>
+                <Button
+                   buttonColour="#f3f2f1"
+                   buttonHoverColour="#ffdd00"
+                   buttonShadowColour="#f47738"
+                   buttonTextColour="#0b0c0c"
+                   style={{margin:" 0px 0px 0px 0px"}}
+                 onClick={() => navigate("/DoctorViewandUpdate", {
+                 state: { PatientNHSNumber: appointment.NHSNumber,
+                 PatientFName:appointment.Forename,
+                 PatientSName:appointment.Surname,
+                 PatientDOB:appointment.PersonDOB,
+                 PatientMedicalNote:appointment.MedicalRecord }})}>
+                  View  Record
+                </Button>
+              </Table.Cell>
+            </Table.Row>
+          ) )}
   
 </Table>
 </Main>
--- react-app/src/Pages/DoctorPages/DoctorViewandUpdate.js
+++ react-app/src/Pages/DoctorPages/DoctorViewandUpdate.js
@@ -1,8 +1,21 @@
-import { Main,Breadcrumbs,H3 } from "govuk-react";
+import { Main,Breadcrumbs,H3,Label,LabelText,InputField } from "govuk-react";
 import Navigation from "../../Components/Navigation";
-function DoctorViewandUpdate(){
-    return(
+import { useLocation } from "react-router-dom";
 
+function DoctorViewandUpdate(){
+
+    const location = useLocation();
+    const NHSNumber = location.state.PatientNHSNumber;
+    const FName = location.state.PatientFName;
+    const SName = location.state.PatientSName;
+    const DOB = location.state.PatientDOB;
+    const MedicalRecord = location.state.PatientMedicalNote;
+
+ 
+
+ 
+
+    return(
         <div>
  <Navigation pageLink1="/" PageName1="Home" pageLink2="/DoctorDashBoard" PageName2="Doctor DashBoard" pageLink3="/DoctorAppointment" PageName3="Doctor List Of Appointments"/>
 
@@ -22,7 +35,20 @@ function DoctorViewandUpdate(){
   <H3 size="LARGE" style={{textAlign: "center"}}>
 View and Update Medical Records  </H3>
 <br/><br/><br/>
+{NHSNumber}
+{FName} {SName}
+{DOB}
+{MedicalRecord}
+<InputField
 
+  input={{
+    name: 'group1'
+  }}
+>
+
+</InputField>
+
+    
             </Main>    
         </div>
     );
