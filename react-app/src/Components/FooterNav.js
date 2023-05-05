b5c437db52b4a4bb65c41242d2eb3556895086f1
Mohamed Yusuf Mohamed <107251702+Mohamed-Y-Mohamed@users.noreply.github.com>
2023-05-05 22:32:59 +0100

    FOOTER UPDATED

--- react-app/src/Components/FooterNav.js
+++ react-app/src/Components/FooterNav.js
@@ -1,3 +1,9 @@
+/**
+ * Author(s) of this code:
+ *
+ * Wallyson Alves Da Silva
+ * Mohamed Mohamed
+ */
 import { Footer } from "govuk-react";
 import "../Styles/footer.css"
 function FooterNav() {
