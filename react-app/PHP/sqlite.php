d8e1116b80df874e6ebfb0d6d19db4d7bf06f98e
LB101 <128667927+LB101@users.noreply.github.com>
2023-05-09 10:22:25 +0100

    Update sqlite.php

--- react-app/PHP/sqlite.php
+++ react-app/PHP/sqlite.php
@@ -1,4 +1,5 @@
 <?php
+// Author of this page Laurence Baboolal
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
